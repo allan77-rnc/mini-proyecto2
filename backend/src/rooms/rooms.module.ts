f6ecebf0a7179ef17a786ba16aab5b4a21a1ff7a
Joel Ochoa <123183117+joler023@users.noreply.github.com>
2026-06-14 19:13:58 -0500

    feat: add messages subcollection, Socket.io gateway and Firestore rules

--- backend/src/rooms/rooms.module.ts
+++ backend/src/rooms/rooms.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FirebaseModule } from '../firebase/firebase.module';
 import { UsersModule } from '../users/users.module';
+import { MessagesRepository } from './repositories/messages.repository';
 import { RoomsRepository } from './repositories/rooms.repository';
 import { ROOMS_REPOSITORY } from './repositories/rooms.repository.interface';
 import { RoomsController } from './rooms.controller';
+import { RoomsGateway } from './rooms.gateway';
 import { RoomsService } from './rooms.service';
 
 @Module({
   imports: [FirebaseModule, UsersModule],
   providers: [
     RoomsService,
+    MessagesRepository,
+    RoomsGateway,
     { provide: ROOMS_REPOSITORY, useClass: RoomsRepository },
   ],
   controllers: [RoomsController],
